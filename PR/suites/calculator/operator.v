28f883c1f5bbbf3f0ad312d0a688401e9c2d19b2
Cl3ms_Maul <clems.maul@tatooin.ta>
2013-04-30 09:39:33 +0200

    Projet PR

---
+++ PR/suites/calculator/operator.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:38:25 02/27/2013 
+// Design Name: 
+// Module Name:    operation 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module operation(
+	input clk_100MHz,
+	input operation_enable,
+	input [7:0] data_in,
+	output [7:0] data_out);
+endmodule
